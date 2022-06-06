fb73e51cc5d9ec8f1bb61be7ec6e6aeb433a043f
newatcoding <lakshay@attentive.ai>
2022-06-06 15:55:53 +0530

    Book 3 last and start book 4

---
+++ Book3(this & object prototypes)/Chapter4(mixing up class Objects)/index.js
@@ -0,0 +1,5 @@
+//Mixing Class Objects
+
+//Read from book directly, too much theory very little data 
+// https://github.com/getify/You-Dont-Know-JS/blob/1st-ed/this%20%26%20object%20prototypes/ch4.md
+// or try javascript.info for more of summary on topic  
