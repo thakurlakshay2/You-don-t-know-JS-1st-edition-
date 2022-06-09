29202549c400b79bb609c47410d84e8f10d2430e
newatcoding <lakshay@attentive.ai>
2022-06-09 14:15:55 +0530

    book 4 chapter 3 , begin chapter 4

---
+++ Book4(types&grammer)/Chapter3(natives)/index.js
@@ -0,0 +1,19 @@
+//Natives (this has barely any content read the document)
+
+var s=new String('arr');     //String object
+console.log(s);
+console.log(typeof s);
+
+console.log('--------------------------------------------------------------------------------------------------');
+
+//unboxing
+var a=new String('abc');
+var b=new Number(123);
+var c=new Boolean(true);
+
+var d=a+'d';          //now object 'a' adding to string 'd' returns a string 
+console.log(typeof d);
+console.log(typeof a);
+
+
+//read from the document i believe it is mostly useless
